29053b252cb80ecaf985b66df19a0f7dd896c176
Mauro Navarro <mauronav006@gmail.com>
2020-09-23 19:30:54 -0600

    Main e interfaz (Añadido)
    Se añada una clase main con partes de la interfaz que se correrá por medio de un hilo.

---
+++ src/Main.java
@@ -0,0 +1,137 @@
+import Sockets.ChatClient;
+import Sockets.ChatServer;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import Sockets.ChatClient;
+import Sockets.ChatServer;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+import java.net.UnknownHostException;
+import java.util.*;
+
+public class Main extends Application {
+
+    Pane mainPane;
+    Scene menuScene;
+    Scene messageScene;
+    VBox centralPane;
+    int hostingPort;
+
+    ChatServer server;
+    ChatClient client;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //new SocketHosting();
+
+        primaryStage.setTitle("Socket Chat");
+        primaryStage.setWidth(800);
+        primaryStage.setHeight(650);
+        primaryStage.setResizable(false);
+        primaryStage.getIcons().add(new Image("Images/logo.png"));
+
+        mainPane = new Pane();
+        mainPane.setPrefSize(800,650);
+
+        centralPane = new VBox(10);
+        centralPane.setPrefSize(800,560);
+        centralPane.setMaxWidth(Region.USE_PREF_SIZE);
+        centralPane.setMaxHeight(Region.USE_PREF_SIZE);
+        centralPane.setBackground(Background.EMPTY);
+        String style = "-fx-background-color: rgba(255, 255, 255, 0.5);";
+        centralPane.setStyle(style);
+        centralPane.setLayoutX(0);
+        centralPane.setLayoutY(0);
+
+        //SCENE 2
+        Label portLabel = new Label("Puerto:");
+        portLabel.setPrefSize(170,58);
+        portLabel.setFont(new Font("System",30));
+        TextField portTextField = new TextField();
+        portTextField.setPrefSize(300,66);
+
+        Label ipLabel = new Label("IP:");
+        ipLabel.setPrefSize(170,58);
+        ipLabel.setFont(new Font("System",30));
+        TextField ipTextField = new TextField();
+        ipTextField.setPrefSize(300,66);
+        HBox topHbox = new HBox();
+        topHbox.setPrefSize(800,66);
+        topHbox.getChildren().addAll(portLabel,portTextField,ipLabel,ipTextField);
+
+        Pane fillingPane = new Pane();
+        fillingPane.setPrefSize(800,103);
+
+        TextField messageTextField = new TextField();
+        messageTextField.setFont(new Font("System",18));
+        messageTextField.setPromptText("Escriba su mensaje aqui");
+        messageTextField.setAlignment(Pos.TOP_LEFT);
+        messageTextField.setPrefSize(691,389);
+
+        Pane fillingPane2 = new Pane();
+        fillingPane2.setPrefSize(671,98);
+        Button sendButton = new Button("Enviar");
+        sendButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                send(ipTextField.getText(), portTextField.getText(), messageTextField.getText());
+                } catch (UnknownHostException e) {
+                e.printStackTrace();
+                }
+                ipTextField.setText("");
+                messageTextField.setText("");
+                primaryStage.setScene(menuScene);
+                //centralPane.getChildren().add();
+                }
+        });
+        sendButton.setFont(new Font("System",20));
+        sendButton.setPrefSize(103,60);
+        HBox bottomHbox = new HBox();
+        bottomHbox.setPrefSize(200,100);
+        bottomHbox.getChildren().addAll(fillingPane2,sendButton);
+
+
+        VBox layout1 = new VBox();
+        layout1.setPrefSize(800,650);
+        layout1.getChildren().addAll(topHbox,fillingPane,messageTextField,bottomHbox);
+        Scene scene2 = new Scene(layout1,800,650);
+        messageScene = scene2;
+        //FIN DE SCENE 2
+
+        Button sendBtn= new Button("Nuevo Mensaje");
+        sendBtn.setPrefSize(112,46);
+        sendBtn.setLayoutX(333);
+        sendBtn.setLayoutY(510);
+        sendBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                    primaryStage.setScene(messageScene);
+                    }
+        });
+
+        mainPane.getChildren().addAll(centralPane,sendBtn);
+        Scene mainScene = new Scene (mainPane,344,570);
+        menuScene = mainScene;
+
+
+        primaryStage.setScene(mainScene);
+
+        primaryStage.show();
+        }
+
+    public static void main(String args[]){launch(args);}
+}
